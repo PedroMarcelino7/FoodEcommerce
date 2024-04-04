6873c24e1e3e302dd62b99b7130c97b067e50f54
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-04 18:05:20 -0300

    Including assets [icons]

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,25 @@
 import { Container } from "./styles";
 
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
+
 export function Sidebar() {
     return (
         <Container>
-            
+            <button>
+                <img src={menuImg} alt="Menu" />
+            </button>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <BurgerIcon />
+                            <span>Hambúrgueres</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </Container>
     )
 }
