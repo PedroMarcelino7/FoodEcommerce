77c88ebeddb8efee714cc7e2acf047c15e2bfdf6
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-04 16:54:16 -0300

    Starting sidebar

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,13 @@
 import { Container } from './styles'
 
+import { Sidebar } from '../../components/Sidebar'
+
 export default function Main() {
   return (
     <Container>
-      
+      <Sidebar />
+
+      <h1>main</h1>
     </Container>
   )
 }
