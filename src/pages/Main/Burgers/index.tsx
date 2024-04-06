b7a3334aa2df2ec25d3208ce20f136c6a2d13486
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-05 23:19:57 -0300

    creating burger list

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -3,6 +3,25 @@ import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
 
 export default function Burgers() {
+    const data = [
+        {
+            id: 1,
+            snack: 'burger',
+            name: 'Mega',
+            description: 'Descrição personalizada 1',
+            price: 25.5,
+            image: 'https://i.imgur.com/upjIUnG.jpg'
+        },
+        {
+            id: 2,
+            snack: 'burger',
+            name: 'Extra bacon',
+            description: 'Descrição personalizada 2',
+            price: 23.5,
+            image: 'https://i.imgur.com/B4J04AJ.jpg'
+        }
+    ]
+
     return (
         <>
             <Head title="Burgers" description="Nossos melhores burgers" />
