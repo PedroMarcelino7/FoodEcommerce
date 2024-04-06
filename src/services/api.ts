495186c8ff1aafb2c4eb4b0d3551e50395830ffb
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-06 16:16:57 -0300

    Creating api get functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,4 +5,9 @@ const api = axios.create({
   baseURL: 'http://localhost:5000',
 })
 
+export const getBurgers = () => api.get('/burgers')
+export const getPizzas = () => api.get('/pizzas')
+export const getDrinks = () => api.get('/drinks')
+export const getIceCreams = () => api.get('/ice-creams')
+
 export default api
