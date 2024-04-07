378418aa1d9e6677711a1029ff50a32afd546d18
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-07 01:40:37 -0300

    Creating basic loading

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -10,20 +10,22 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps) {
     return (
         <Container>
-            {snacks.map((snack) => (
-                <div key={snack.id} className="snack">
-                    <h2>{snack.name}</h2>
-                    <img src={snack.image} alt={snack.name} />
-                    <p>{snack.description}</p>
+            {!snacks.length
+                ? <p style={{ color: '#fff' }}>Loading...</p>
+                : snacks.map((snack) => (
+                    <div key={snack.id} className="snack">
+                        <h2>{snack.name}</h2>
+                        <img src={snack.image} alt={snack.name} />
+                        <p>{snack.description}</p>
 
-                    <div>
-                        <strong>{currencyFormat(snack.price)}</strong>
-                        <button type="button">
-                            <FiPlus />
-                        </button>
+                        <div>
+                            <strong>{currencyFormat(snack.price)}</strong>
+                            <button type="button">
+                                <FiPlus />
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
         </Container>
     )
 }
