e0fc0b49d80540afe23e1309ecc04546dc102129
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-08 15:27:35 -0300

    Creating cart button

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,16 +5,19 @@ import { Container } from './styles'
 import { Sidebar } from '../../components/Sidebar'
 
 import logoImg from '../../assets/logo.svg'
+import { MyOrder } from '../../components/MyOrder'
 
 export default function Main() {
   return (
     <Container>
       <Sidebar />
-      
+
       <section>
         <img src={logoImg} alt="Logo" />
         <Outlet />
       </section>
+
+      <MyOrder />
     </Container>
   )
 }
