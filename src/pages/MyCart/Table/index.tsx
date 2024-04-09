6553e6c2090ff9ca0470b967954e9a43b65f60b2
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-09 14:11:49 -0300

    Conditional rendering for responsiveness

--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react"
+
 import { useCart } from "../../../hooks/useCart"
+
 import { TableDesktop } from "./TableDesktop"
+import { TableMobile } from "./TableMobile"
 
 export function Table() {
+    const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
+
     const { cart } = useCart()
+
+    useEffect(() => {
+        function updateTableComponentBasedInWindowWidth() {
+            const currentWidth = document.documentElement.clientWidth
+
+            setWindowWidth(currentWidth)
+        }
+
+        window.addEventListener('resize', updateTableComponentBasedInWindowWidth)
+
+        return () => {
+            window.removeEventListener('resize', updateTableComponentBasedInWindowWidth)
+        }
+    }, [])
 
     if (cart.length === 0) {
         return <h1>Ops... Não há pedidos!</h1>
     }
 
     return (
-        <TableDesktop />
+        windowWidth > 768
+        ? <TableDesktop />
+        : <TableMobile />
     )
 }
