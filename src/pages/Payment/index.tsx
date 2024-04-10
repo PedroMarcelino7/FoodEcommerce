44c7eb125f7bfd5b26ad198cb3f4271ff89cbb75
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-10 14:15:25 -0300

    Creating forms field pt.1

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,6 +1,6 @@
 import { Head } from "../../components/Head";
 import { OrderHeader } from "../../components/OrderHeader";
-import { Container, Inner } from "./styles";
+import { Container, Form, Inner } from "./styles";
 
 export default function Payment() {
     return (
@@ -10,7 +10,31 @@ export default function Payment() {
             <OrderHeader />
 
             <Inner>
-                Inner
+                <Form>
+                    <h4>Informações pessoais</h4>
+
+                    <div className="field">
+                        <label htmlFor="full-name">Nome completo</label>
+                        <input type="text" id="full-name" name="full-name" autoComplete="name" />
+                    </div>
+
+                    <div className="grouped">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" id="email" name="email" autoComplete="email" />
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="mobile">Telefone</label>
+                            <input type="tel" id="mobile" name="mobile" autoComplete="phone" />
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="document">CPF / CNPJ</label>
+                            <input type="text" id="document" name="document" />
+                        </div>
+                    </div>
+                </Form>
             </Inner>
         </Container>
     )
