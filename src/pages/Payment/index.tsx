f08a3302bf17c58aa800b99369a2ec9f28c246e3
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-12 23:13:38 -0300

    Finishing front end validation form

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -264,23 +264,101 @@ export default function Payment() {
 
                     <div className="field">
                         <label htmlFor="creditCardNumber">Número do cartão</label>
-                        <input type="text" id="creditCardNumber" name="creditCardNumber" autoComplete="cc-number" />
+                        <Controller
+                            name='creditCardNumber'
+                            control={control}
+                            render={({ field }) => (
+                                <IMaskInput
+                                    type='text'
+                                    id='creditCardNumber'
+                                    mask={[
+                                        {
+                                            mask: '0000 000000 0000',
+                                            maxLength: 14
+                                        },
+                                        {
+                                            mask: '0000 000000 00000',
+                                            maxLength: 15
+                                        },
+                                        {
+                                            mask: '0000 0000 0000 0000'
+                                        }
+                                    ]}
+                                    {...field}
+                                />
+                            )}
+                        />
+
+                        {errors.creditCardNumber &&
+                            <p className='error'>{errors.creditCardNumber.message}</p>
+                        }
                     </div>
 
                     <div className="field">
-                        <label htmlFor="credit-card-holder-number">Nome impresso no cartão</label>
-                        <input type="text" id="credit-card-holder-number" name="credit-card-holder-number" autoComplete="cc-name" />
+                        <label htmlFor="creditCardHolder">Nome impresso no cartão</label>
+                        <Controller
+                            name='city'
+                            control={control}
+                            render={({ field }) => (
+                                <input type='text' id='creditCardHolder' {...field} />
+                            )}
+                        />
+
+                        {errors.creditCardHolder &&
+                            <p className='error'>{errors.creditCardHolder.message}</p>
+                        }
                     </div>
 
-                    <div className="grouped">
-                        <div className="field">
-                            <label htmlFor="credit-card-expiration">Validade (MM/AA)</label>
-                            <input type="text" id="credit-card-expiration" name="credit-card-expiration" autoComplete="cc-exp" />
+                    <div className='grouped'>
+                        <div className='field'>
+                            <label htmlFor='creditCardExpiration'>Validade (MM/AA)</label>
+                            <Controller
+                                name='creditCardExpiration'
+                                control={control}
+                                render={({ field }) => (
+                                    <IMaskInput
+                                        type='text'
+                                        id='creditCardExpiration'
+                                        mask={[
+                                            {
+                                                mask: 'MM/YY',
+                                                blocks: {
+                                                    MM: {
+                                                        mask: IMask.MaskedRange,
+                                                        from: 1,
+                                                        to: 12,
+                                                    },
+                                                    YY: {
+                                                        mask: IMask.MaskedRange,
+                                                        from: new Date().getFullYear() - 2000,
+                                                        to: 99,
+                                                    },
+                                                },
+                                            },
+                                        ]}
+                                        {...field}
+                                    />
+                                )}
+                            />
+
+                            {errors.creditCardExpiration && (
+                                <p className='error'>{errors.creditCardExpiration.message}</p>
+                            )}
                         </div>
 
-                        <div className="field">
-                            <label htmlFor="credit-card-code">Código de segurança (CVV)</label>
-                            <input type="text" id="credit-card-code" name="credit-card-code" autoComplete="cc-csc" />
+                        <div className='field'>
+                            <label htmlFor='creditCardSecurityCode'>Código de segurança (CVV)</label>
+                            <Controller
+                                name='creditCardSecurityCode'
+                                control={control}
+                                render={({ field }) => (
+                                    <IMaskInput type='text' id='creditCardSecurityCode' mask={'0000'} {...field} />
+                                )}
+                            />
+
+                            {errors.creditCardSecurityCode && (
+                                <p className='error'>{errors.creditCardSecurityCode.message}</p>
+                            )}
                         </div>
                     </div>
                     {/* ------ */}
