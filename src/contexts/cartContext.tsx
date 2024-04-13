26dc0068529831eaa6f29a1209cb16a7c5841052
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-13 14:59:38 -0300

    Using localstorage to record items

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -28,9 +28,17 @@ interface CartProviderProps {
 
 export const CartContext = createContext({} as CartContextProps)
 
+const localStorageKey = '@FoodEcommerce:cart'
+
 export function CartProvider({ children }: CartProviderProps) {
     const navigate = useNavigate()
     const [cart, setCart] = useState<Snack[]>([])
+
+    function saveCart(items: Snack[]) {
+        setCart(items)
+
+        localStorage.setItem(localStorageKey, JSON.stringify(items))
+    }
 
     function addSnackIntoCart(snack: SnackData): void {
         const snackExistentInCart = cart.find(
@@ -50,7 +58,7 @@ export function CartProvider({ children }: CartProviderProps) {
             })
 
             toast.success(`Outro(a) ${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos!`)
-            setCart(newCart)
+            saveCart(newCart)
 
             return
         }
@@ -59,13 +67,13 @@ export function CartProvider({ children }: CartProviderProps) {
         const newCart = [...cart, newSnack]
 
         toast.success(`${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos!`)
-        setCart(newCart)
+        saveCart(newCart)
     }
 
     function removeSnackFromCart(snack: Snack) {
         const newCart = cart.filter((item) => !(item.id === snack.id && item.snack === snack.snack))
 
-        setCart(newCart)
+        saveCart(newCart)
     }
 
     function updateSnackQuantity(snack: Snack, newQuantity: number) {
@@ -89,7 +97,7 @@ export function CartProvider({ children }: CartProviderProps) {
             return item
         })
 
-        setCart(newCart)
+        saveCart(newCart)
     }
 
     function snackCartIncrement(snack: Snack) {
